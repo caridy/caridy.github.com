397b1b10235b812a892b305b5c7f8d485be5b348
Caridy Patino <caridy@gmail.com>
2010-06-03 15:33:03 -0400

    adding the formal documentation

--- examples/js-demo/event-binder.js
+++ examples/js-demo/event-binder.js
@@ -1,38 +1,58 @@
 YUI.add('gallery-event-binder', function(Y) {
 
 /**
-* <p>The Dispatcher satisfies a very common need of developers using the 
-* YUI library: dynamic execution of HTML Fragments or remote content. Typical strategies to 
-* fulfill this need, like executing the innerHTML property or referencing remote 
-* scripts, are unreliable due to browser incompatibilities. The Dispatcher normalize 
-* this behavior across all a-grade browsers.
+* <p>The Event Binder satisfies a very specific need. Binding user actions until a 
+* particular YUI instance become ready, and the listeners defined before flushing those 
+* events through a queue. This will help to catch some early user interactions due 
+* the ondemand nature of YUI 3. 
 * 
-* <p>To use the Dispatcher Module, simply create a new object based on Y.Dispatcher
-* and pass a reference to a node that should be handled.</p>
+* <p>To use the Event Binder Module, you have to leverage YUI_config object first. More information
+* about this object visit this page: http://developer.yahoo.com/yui/3/api/config.html<p>
+* <p>There is a member of this global object that you have to set up, the member is called: "eventbinder". To get
+* more information about this object, visit this page: http://yuilibrary.com/gallery/show/event-binder</p>
 * 
 * <p>
 * <code>
 * &#60;script type="text/javascript"&#62; <br>
 * <br>
-*		//	Call the "use" method, passing in "gallery-dispatcher".	 This will <br>
-*		//	load the script for the Dispatcher Module and all of <br>
-*		//	the required dependencies. <br>
+*		//	Call the "use" method, passing in "gallery-event-binder".	 Then you can<br>
+*		//	call Y.EventBinder.flush('click'); to flush all the events that might had happened<br>
+*		//	before your listeners were defined. <br>
 * <br>
-*		YUI().use("gallery-dispatcher", function(Y) { <br>
+*		YUI().use("gallery-event-binder", "event", function(Y) { <br>
 * <br>
-*			(new Y.Dispatcher ({<br>
-*				node: '#demoajax',<br>
-*				content: 'Please wait... (Injecting fragment.html)'<br>
-*			})).set('uri', 'fragment.html');<br>
+*			Y.on('click', function(e) {<br>
+*				// do your stuff here...<br>
+*			}, '#demo');<br>
+* <br>
+*			Y.EventBinder.flush('click');<br>
+* <br>
+*		});<br>
+*});<br>
 * <br>
 * <br>		
 *	&#60;/script&#62; <br>
 * </code>
 * </p>
 *
-* <p>The Dispatcher has several configuration properties that can be 
-* set via an object literal that is passed as a first argument during the
-* initialization, or using "set" method.
+* <p>The Event Binder has a single method called "flush". This method accept one argument to
+* identify what type of event should be flushed. The argument can be:</p>
+* <ul>
+* <li>click</li>
+* <li>dblclick</li>
+* <li>mouseover</li>
+* <li>mouseout</li>
+* <li>mousedown</li>
+* <li>mouseup</li>
+* <li>mousemove</li>
+* <li>keydown</li>
+* <li>keyup</li>
+* <li>keypress</li>
+* <li>...etc...</li>
+* </ul>  
+* <p>Keep in mind that before flushing any of these events, you have to add them to the 
+* monitoring system through the configuration object (YUI_config.eventbinder), otherwise
+* YUI will be unable to listen for any early user interaction.</p>
 * </p>
 *
 * @module gallery-event-binder
@@ -40,15 +60,15 @@ YUI.add('gallery-event-binder', function(Y) {
 Y.EventBinder = {
 	flush: function (type) {
 		var config = Y.config.eventbinder || {};
-		
+
 		config.q = config.q || [];
 		type = type || 'click';
-		
+
 		if (config.fn) {
 			Y.Event.detach(type, config.fn, Y.config.doc);
 		}
 		Y.each(config.q, function(o) {
-			
+
 			if (type == o.type) {
 				Y.get(o.target).removeClass('yui3-waiting');
 				Y.Event.simulate(o.target, type, o);
@@ -56,6 +76,5 @@ Y.EventBinder = {
 		});
 	}
 };
-
-
+	
 }, '@VERSION@' ,{requires:['event-simulate','event-base']});
