453a4cc334560c71b28c16f577777ad44dba3b19
Caridy Patino <caridy@gmail.com>
2010-06-22 19:40:43 -0400

    adding a new example

--- examples/gallery-event-binder/my-custom-module.js
+++ examples/gallery-event-binder/my-custom-module.js
@@ -1,17 +1,17 @@
 YUI.add('my-custom-module', function (Y) {
 	
-	Y.one('#output').append('<li>[my-cutom-module] was loaded and it is ready to be initialized</li>');
+	Y.one('#output').append('<li>[my-custom-module] was loaded and initialized</li>');
 	
 	Y.on('click', function(e) {
 		
-		Y.one('#output').append('<li>Listener for [#democlick a] on click executed.');
+		Y.one('#output').append('<li>[#democlick a] click executed.');
 		e.halt();
 		
 	}, '#democlick a')
 	
 	Y.delegate('click', function(e) {
 		
-		Y.one('#output').append('<li>Listener for [#demodelegate a] using delegate executed.');
+		Y.one('#output').append('<li>[#demodelegate a] delegate executed.');
 		e.halt();
 		
 	}, '#demodelegate', 'a')
--- examples/gallery-event-binder/my-yui-config.js
+++ examples/gallery-event-binder/my-yui-config.js
@@ -1,65 +1,75 @@
 YUI_config = {
-	eventbinder: {
-		// set of options that should be preserved for every event (they are all optional)
-		ev: {
-			ctrlKey: 0,
-			altKey: 0,
-			shiftKey: 0,
-			metaKey: 0,
-			keyCode: 0,
-			charCode: 0,
-			screenX: 0,
-			screenY: 0,
-			clientX: 0,
-			clientY: 0,
-			button: 0,
-			relatedTarget: 0
-		},
-		// listener callback function
-		fn: function(e) {
-			var o = YUI_config.eventbinder,
-				filter = /yui3-event-binder/, i,
-				t = (e.target ? e.target: e.srcElement),
-				p = {
-					target: t,
-					type: e.type
-				},
-				container = t;
-		
-			// looking for an element with the class yui3-event-binder
-			while (container && !filter.test(container.className)) {
-				container = container.parentNode;
-			}
-		
-			if (container) {
-				t.className += ' yui3-waiting';
-			
-				// backing up the event properties to simulate the event later on
-				for (i in (o.ev || {})) {
-					if (o.ev.hasOwnProperty(i)) {
-						p[i] = e[i];
-					}
-				}
-				(o.q = o.q || []).push(p);
-			
-				// preventing the default browser action for this event
-				if (e.preventDefault) {
-					e.preventDefault();
-				}
-				return (e.returnValue = false);
-			}
-		},
-		add: function(type) {
-			var d = document;
-			if (d.addEventListener) {
-				d.addEventListener(type, this.fn, false);
-			}
-			else {
-				d.attachEvent('on' + type, this.fn);
-			}
-			return this;
-		}
-	}
+    // standard YUI_config configuration
+    combine: true,
+    filter: 'min',
+
+    // event binder configuration starts here
+    eventbinder: {
+        // set of options that should be preserved for every event (all optional)
+        eventProperties: [
+            "ctrlKey", "altKey",
+            "shiftKey", "metaKey",
+            "keyCode", "charCode",
+            "screenX", "screenY",
+            "clientX", "clientY",
+            "button",
+            "relatedTarget"
+        ],
+
+        // listener callback function
+        fn: function(e) {
+            var binder = YUI_config.eventbinder,
+                props = binder.eventProperties,
+                filter = /yui3-event-binder/,
+                target = (e.target || e.srcElement),
+                container = target,
+                info = {
+                    target: target,
+                    type : e.type
+                },
+                i;
+
+            if (target.nodeType === 3) {
+                // target is a text node, so use its parent element
+                target = target.parentNode;
+            }
+
+            // look for an element with the class yui3-event-binder
+            while (container && !filter.test(container.className)) {
+                container = container.parentNode;
+            }
+
+            if (container) {
+                target.className += ' yui3-waiting';
+
+                // back up the event properties to simulate the event later on
+                for (i = props.length - 1; i >= 0; --i) {
+                    info[props[i]] = e[props[i]];
+                }
+
+                (binder.q = binder.q || []).push(info);
+
+                // prevent the default browser action for this event
+                if (e.preventDefault) {
+                    e.preventDefault();
+                }
+                return (e.returnValue = false);
+            }
+        },
+
+        add: function(type) {
+            var d = document;
+
+            if (d.addEventListener) {
+                d.addEventListener(type, this.fn, false);
+            } else {
+                d.attachEvent('on' + type, this.fn);
+            }
+
+            return this;
+        }
+    }
 };
-// adding events to the monitoring process
+
+// add events to the monitoring process
 YUI_config.eventbinder.add('click');
